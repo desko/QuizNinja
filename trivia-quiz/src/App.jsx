78e1abf7622aa612171cae4d5e0b823263dbf037
Radostin Radev <radoks95@gmail.com>
2023-06-28 16:38:54 +0300

    Add a Logo

--- trivia-quiz/src/App.jsx
+++ trivia-quiz/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
+import Logo from './components/Logo/Logo';
 
 function App() {
   return (
     <div className="wrapper">
+      <Logo />
       <h1>Hello</h1>
     </div>
   );
