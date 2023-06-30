93f6d3629ebdfa89fbfd6dca494eba0dc5d6ec47
desislavdimitrov <desislavdimitrov@htmlbox.net>
2023-06-30 23:08:41 +0300

    container media queries

--- trivia-quiz/src/components/Container/Container.jsx
+++ trivia-quiz/src/components/Container/Container.jsx
@@ -3,7 +3,11 @@ import {Center, Box} from '@chakra-ui/react';
 const Container = ( {children} ) => {
   return (
     <Center>
-      <Box maxWidth='100rem' paddingX='2rem'>
+      <Box maxWidth={{'sm': '100%',
+        'md': '100%',
+        'lg': '100%',
+        'xl': '100rem',
+        '2xl': '120rem'}} paddingX='2rem'>
         {children}
       </Box>
     </Center>
