97cae4086168afa183f34faef1794dc9ff75a9f3
Radostin Radev <radoks95@gmail.com>
2023-07-03 00:01:42 +0300

    Add an alert on QuizPage refresh

--- trivia-quiz/src/pages/QuizPage/QuizPage.jsx
+++ trivia-quiz/src/pages/QuizPage/QuizPage.jsx
@@ -1,8 +1,39 @@
+import {useContext, useEffect} from 'react';
 import QuizController from '../../components/QuizController/QuizController';
 import {useTitle} from '../../hooks/useTitle';
+import {QuizContext} from '../../context/QuizContext';
+import {useNavigate} from 'react-router-dom';
+import {LANDING_PAGE} from '../../common/routes';
+
 
 const QuizPage = () => {
+  const {quizData} = useContext(QuizContext);
+  const navigate = useNavigate();
+
   useTitle(`QuizNinja - Slashing through question`);
+  useEffect(() => {
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
+  const handleBeforeUnload = (e) => {
+    e.preventDefault();
+    const message =
+      'Are you sure you want to leave? All provided data will be lost.';
+    e.returnValue = message;
+    return message;
+  };
+
+  useEffect(() => {
+    if (quizData.length === 0) {
+      navigate(LANDING_PAGE);
+    }
+  }, [quizData.length, navigate]);
+
+  if (quizData.length === 0) return;
+
   return (
     <>
       <QuizController />
