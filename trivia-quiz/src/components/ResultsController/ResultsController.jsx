073aa18a7a288193e94941fa804f71b7e1bbc5db
Radostin Radev <radoks95@gmail.com>
2023-07-04 21:20:57 +0300

    Add timer to context

--- trivia-quiz/src/components/ResultsController/ResultsController.jsx
+++ trivia-quiz/src/components/ResultsController/ResultsController.jsx
@@ -5,9 +5,10 @@ import {SCORE_MAP} from '../../common/constants';
 import Award from '../../components/Award/Award';
 import AccordionSingle from '../../components/AccordionSingle/AccordionSingle';
 import QuestionReview from '../../components/QuestionReview/QuestionReview';
+import {timeConverter} from '../../common/helpers';
 
 const ResultsController = () => {
-  const {quizData} = useContext(QuizContext);
+  const {quizData, quizTime, quizSubmitted} = useContext(QuizContext);
   const correctAnswers = quizData.filter((question) => {
     return question.correct_answer === question.selectedAnswer;
   });
@@ -15,6 +16,7 @@ const ResultsController = () => {
   const percentageScore = (correctAnswers.length / quizData.length) * 100;
   const percentageThreshhold = Math.floor(percentageScore / 10) * 10;
   const color = SCORE_MAP.find((score) => score.SCORE_TRESHHOLD === percentageThreshhold).COLOR;
+  const remainingTime = timeConverter(quizTime - quizSubmitted);
 
   return (
     <Card
@@ -39,6 +41,18 @@ const ResultsController = () => {
           fontWeight='700'
           textAlign='center'>
             You got {Math.round(percentageScore)} % of the questions right
+        </Heading >
+        <Heading
+          mt='2rem'
+          as='h2'
+          color='orange.600'
+          fontWeight='700'
+          textAlign='center'>
+                        Time remained: {
+            (remainingTime.hours > 9 ? remainingTime.hours : `0${remainingTime.hours}`) + ' : ' +
+          (remainingTime.minutes > 9 ? remainingTime.minutes : `0${remainingTime.minutes}`) + ' : ' +
+          (remainingTime.seconds > 9 ? remainingTime.seconds : `0${remainingTime.seconds}`)
+          }
         </Heading >
         <Heading
           mt='2rem'
--- trivia-quiz/src/context/QuizContext.jsx
+++ trivia-quiz/src/context/QuizContext.jsx
@@ -5,9 +5,18 @@ export const QuizContext = createContext();
 export const QuizProvider = ({children}) => {
   const [quizData, setQuizData] = useState([]);
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [quizTime, setQuizTime] = useState(0);
+  const [quizSubmitted, setQuizSubmitted] = useState(0);
 
   return (
-    <QuizContext.Provider value={{quizData, setQuizData, isSubmitted, setIsSubmitted}}>
+    <QuizContext.Provider value={{quizData,
+      setQuizData,
+      isSubmitted,
+      setIsSubmitted,
+      quizTime,
+      setQuizTime,
+      quizSubmitted,
+      setQuizSubmitted}}>
       {children}
     </QuizContext.Provider>
   );
