bc04a0b39b98ebc75ebc55ea2190d102d412c75c
Radostin Radev <radoks95@gmail.com>
2023-07-06 19:26:52 +0300

    Export logic for alert in a custom hook - useAlert

--- trivia-quiz/src/pages/QuizPage/QuizPage.jsx
+++ trivia-quiz/src/pages/QuizPage/QuizPage.jsx
@@ -4,6 +4,7 @@ import {useTitle} from '../../hooks/useTitle';
 import {QuizContext} from '../../context/QuizContext';
 import {useNavigate} from 'react-router-dom';
 import {LANDING_PAGE} from '../../common/routes';
+import {useAlert} from '../../hooks/useAlert';
 
 
 const QuizPage = () => {
@@ -11,20 +12,7 @@ const QuizPage = () => {
   const navigate = useNavigate();
 
   useTitle(`QuizNinja - Slashing through question`);
-  useEffect(() => {
-    window.addEventListener('beforeunload', handleBeforeUnload);
-    return () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload);
-    };
-  }, []);
-
-  const handleBeforeUnload = (e) => {
-    e.preventDefault();
-    const message =
-      'Are you sure you want to leave? All provided data will be lost.';
-    e.returnValue = message;
-    return message;
-  };
+  useAlert('Are you sure you want to leave? All provided data will be lost.');
 
   useEffect(() => {
     if (quizData.length === 0) {
