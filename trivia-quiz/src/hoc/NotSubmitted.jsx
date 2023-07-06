17ecf7267bd677e0f2625a270890ee1abec55aa6
Radostin Radev <radoks95@gmail.com>
2023-07-06 19:36:04 +0300

    Move logic from QuizPage to the NotSubmitted HOC to limit access to this page if there is no quizData

--- trivia-quiz/src/hoc/NotSubmitted.jsx
+++ trivia-quiz/src/hoc/NotSubmitted.jsx
@@ -1,19 +1,26 @@
 import {useContext, useEffect} from 'react';
 import {QuizContext} from '../context/QuizContext';
 import {useNavigate} from 'react-router-dom';
-import {RESULTS_PAGE} from '../common/routes';
+import {RESULTS_PAGE, LANDING_PAGE} from '../common/routes';
 
 const NotSubmitted = ({children}) => {
-  const {isSubmitted} = useContext(QuizContext);
+  const {quizData, isSubmitted} = useContext(QuizContext);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (isSubmitted) {
       navigate(RESULTS_PAGE);
     }
-  }, [isSubmitted, navigate]);
+    if (quizData.length === 0) {
+      navigate(LANDING_PAGE);
+    }
+  }, [isSubmitted, navigate, quizData.length]);
 
   if (isSubmitted) {
+    return;
+  }
+
+  if (quizData.length === 0) {
     return;
   }
 
--- trivia-quiz/src/pages/QuizPage/QuizPage.jsx
+++ trivia-quiz/src/pages/QuizPage/QuizPage.jsx
@@ -1,26 +1,11 @@
-import {useContext, useEffect} from 'react';
 import QuizController from '../../components/QuizController/QuizController';
 import {useTitle} from '../../hooks/useTitle';
-import {QuizContext} from '../../context/QuizContext';
-import {useNavigate} from 'react-router-dom';
-import {LANDING_PAGE} from '../../common/routes';
 import {useAlert} from '../../hooks/useAlert';
 
 
 const QuizPage = () => {
-  const {quizData} = useContext(QuizContext);
-  const navigate = useNavigate();
-
   useTitle(`QuizNinja - Slashing through question`);
   useAlert('Are you sure you want to leave? All provided data will be lost.');
-
-  useEffect(() => {
-    if (quizData.length === 0) {
-      navigate(LANDING_PAGE);
-    }
-  }, [quizData.length, navigate]);
-
-  if (quizData.length === 0) return;
 
   return (
     <>
